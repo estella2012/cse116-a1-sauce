7be47bf90dd431cc37395ad04d5a08a337ad052b
Derek <Derek@DereksLaptop>
2018-02-17 16:18:04 -0500

    I'm ashamed because this push is just a small format change

--- src/code/Board.java
+++ src/code/Board.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.Random;
 
 
 public class Board {
