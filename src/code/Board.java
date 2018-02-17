8013b00ebd5798f7e9900bdf9ef30f34ac77781e
Isabela Lago <Isabela Lago@Isabela>
2018-02-17 15:07:43 -0500

    Board in progress

---
+++ src/code/Board.java
@@ -0,0 +1,15 @@
+package code;
+
+public class Board {
+	
+	//constructor for Board
+	public Board() {
+		
+	}
+	
+	public Location[][] createBoard() {
+		Location[][] board = new Location[][];
+		return null;
+		
+	}
+}
