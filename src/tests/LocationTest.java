58c9ce11d389a5eab9366bb919f3bde716ccade2
ewesa <ewesa@LAPTOP-DK731PL4>
2018-02-17 15:49:04 -0500

    location test update

--- src/tests/LocationTest.java
+++ src/tests/LocationTest.java
@@ -9,7 +9,7 @@ import code.Location;
 public class LocationTest {
 	
 	
-	
+	Location test = new Location("","");
 
 	@Test
 	public void testLocation() {
@@ -17,13 +17,30 @@ public class LocationTest {
 	}
 	
 	@Test
-	public void getLocationType() {
+	public void testSetPerson() {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void setLocationType() {
+	public void testGetPerson() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testGetCodename() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetCodename() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNotRevealed() {
+		test.setNotRevealed(false);
+		assertFalse(test.isNotRevealed());
+	}
+	
 
 }
