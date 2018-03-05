04211a8db231e3e91af68e49a078cc1c2cd7213d
DerkDurski <derekrod@buffalo.edu>
2018-03-05 15:49:21 -0500

    Started adding clue and count input

--- src/GUIs/MainGUI.java
+++ src/GUIs/MainGUI.java
@@ -2,17 +2,21 @@ package GUIs;
 
 import code.Board;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 
 public class MainGUI {
+	
+	private static String clue;
 	
 	@SuppressWarnings("serial")
 	public static void run() {
@@ -31,8 +35,62 @@ public class MainGUI {
 	    Board board = new Board();
 	    board.createBoard();
 	    board.gameStart("src/GameWords.txt");
-		JFrame frame = new JFrame("CODENAMES");	    
-		JPanel gameWindow = new JPanel();	
+		JFrame frame = new JFrame("CODENAMES");
+		frame.setLayout(new BorderLayout());
+		JPanel gameWindow = new JPanel();
+		JPanel buttonPlace = new JPanel();
+		
+	    JMenu clueShower = new JMenu("");
+	    JMenu countShower = new JMenu("");
+		
+		JButton submitClue = new JButton("Submit Clue");
+		buttonPlace.add(submitClue);
+		
+		submitClue.addActionListener(new ActionListener(){
+	    	@Override
+	        public void actionPerformed(ActionEvent e){
+	    		Object[] possibilities = null;
+	    		String s = (String)JOptionPane.showInputDialog(
+	    		                    frame,
+	    		                    "Type your clue below:\n",
+	    		                    "Customized Dialog",
+	    		                    JOptionPane.PLAIN_MESSAGE,
+	    		                    null,
+	    		                    possibilities,
+	    		                    "");
+	    		while(!(s == null) && board.checkIllegalClue(s)) {
+	    			possibilities = null;
+		    		s = (String)JOptionPane.showInputDialog(
+		    		                    frame,
+		    		                    "Illegal Clue, please choose another:\n",
+		    		                    "Customized Dialog",
+		    		                    JOptionPane.PLAIN_MESSAGE,
+		    		                    null,
+		    		                    possibilities,
+		    		                    "");
+	    		}
+	    		clue = s;
+	    		clue = clue.toUpperCase();
+	    		clueShower.setText("Clue: " + clue);
+	    		Object[] possibilities2 = {"1","2","3","4","5","6","7","8","9"};
+	    		if(!board.isRedTeamTurn()) {
+	    			possibilities2[8] = null;
+	    		}
+				s = (String)JOptionPane.showInputDialog(
+	    		                    frame,
+	    		                    "What is the count?\n",
+	    		                    "Customized Dialog",
+	    		                    JOptionPane.PLAIN_MESSAGE,
+	    		                    null,
+	    		                    possibilities2,
+	    		                    "");
+				if(s.equals("1")) {
+					board.setCount(1);
+				}
+	    		countShower.setText("Count: " + s);
+	    	}
+	    });
+		
 	    JMenuBar menuBar = new JMenuBar();
 	    JMenu menu = new JMenu("File");
 	    JMenu turn = new JMenu();
@@ -82,7 +140,10 @@ public class MainGUI {
 	    
 	    menu.add(newGame);
 	    menu.add(exit);
-	    menuBar.add(menu);
+	    menuBar.add(menu);   
+	    menuBar.add(Box.createHorizontalGlue());
+	    menuBar.add(clueShower);
+	    menuBar.add(countShower);
 	    menuBar.add(Box.createHorizontalGlue());
 	    menuBar.add(turn);
 	    frame.add(menuBar);
@@ -111,7 +172,8 @@ public class MainGUI {
 			gameWindow.add(buttonToAdd);
 		}
 		
-		frame.add(gameWindow);	    
+		frame.add(gameWindow, BorderLayout.CENTER);
+		frame.add(buttonPlace, BorderLayout.PAGE_END);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setVisible(true);
 	    frame.pack();
