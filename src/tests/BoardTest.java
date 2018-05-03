976da50faf10173cad54b5525f8dcb838081bfe3
ewesa <ewesa@LAPTOP-DK731PL4>
2018-05-03 19:53:18 -0400

    BoardTest is fixed but needs more update

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import code.Board;
 
 public class BoardTest {
-	
+
 	/*
 	 * Tests that createListOfWords picks 25 random words from the list.
 	 */
@@ -41,23 +41,23 @@ public class BoardTest {
 		list2.add("isacc"); 
 		list2.add("derek"); 
 		list2.add("ishmam"); */
-		
+
 
 		ArrayList<String> list = new ArrayList<String>();
 		Board bob = new Board();
 		list = bob.createListOfWords("src/GameWords.txt");
-		
+
 		String tester = "AFRICAAGENTAIRALIENALPSAMAZON";
 		String testee = "";
-		
+
 		for(int i = 0; i < 5; i++) {
 			testee += list.get(i);
 		}
-		
+
 		assertEquals(25, list.size());
 		assertFalse(tester.equals(testee));
 	}
-	
+
 	/*
 	 * Checks that createListOfPersons makes a randomized list of assignments.
 	 */
@@ -69,16 +69,16 @@ public class BoardTest {
 		//test list size
 		assertEquals(25, list.size());
 		//test shuffle
-		
+
 		String tester = "redredredredred";
 		String testee = "";
-		
+
 		for(int i = 0; i < 5; i++) {
 			testee += list.get(i);
 		}
 		assertFalse(tester.equals(testee));
 	}
-	
+
 	/*
 	 * Checks that gameStart sets the turn correctly.
 	 */
@@ -86,11 +86,11 @@ public class BoardTest {
 	public void testGameStart() {
 		Board bob = new Board();
 		bob.createBoard();
-		bob.setRedTeamTurn(false);
 		bob.gameStart("src/GameWords.txt");
-		assertTrue(bob.isRedTeamTurn());	
+		assertTrue(bob.getTurnCount() == 0);
+		assertEquals(0, bob.getCount());
 	}
-	
+
 	/*
 	 * Checks if checkIllegalClue can properly determine whether or not a clue is legal.
 	 */
@@ -103,7 +103,7 @@ public class BoardTest {
 		br.getBoard()[5].setCodename("number");
 		assertTrue(br.checkIllegalClue(br.getBoard()[0].getCodename()));
 	}
-	
+
 	/*
 	 * Tests that gameWon can determine if the board is in a winning state.
 	 */
@@ -112,29 +112,38 @@ public class BoardTest {
 		Board br = new Board();
 		br.gameStart("src/GameWords.txt");
 		assertFalse(br.gameWon());
+
 		br.setBluesLeft(5);
 		br.setRedsLeft(0);
+		br.setGreensLeft(1);
 		assertTrue(br.gameWon());
+
 		br.setBluesLeft(0);
 		br.setRedsLeft(5);
+		br.setGreensLeft(1);
+		assertTrue(br.gameWon());
+
+		br.setBluesLeft(1);
+		br.setRedsLeft(5);
+		br.setGreensLeft(0);
 		assertTrue(br.gameWon());
 	}
-	
+
 	/*
 	 * Tests that the turn can change.
 	 */
-	@Test
-	public void testSetRedTeamTurn() {
-		Board br = new Board();
-		br.createBoard();
-		br.gameStart("src/GameWords.txt");
-		assertTrue(br.isRedTeamTurn());
-		br.setRedTeamTurn(false);
-		assertFalse(br.isRedTeamTurn());
-		br.setRedTeamTurn(true);
-		assertTrue(br.isRedTeamTurn());
-	}
-	
+	//	@Test
+	//	public void testSetRedTeamTurn() {
+	//		Board br = new Board();
+	//		br.createBoard();
+	//		br.gameStart("src/GameWords.txt");
+	//		assertTrue(br.getTeamCount() == 0);
+	//		br.
+	//		assertFalse(br.isRedTeamTurn());
+	//		br.setRedTeamTurn(true);
+	//		assertTrue(br.isRedTeamTurn());
+	//	}
+
 	/*
 	 * Checks that whichTeamWonAssassin will return the proper team in the case of the Assassin being chosen.
 	 */
@@ -143,39 +152,53 @@ public class BoardTest {
 		Board br = new Board();
 		br.createBoard();
 		br.gameStart("src/GameWords.txt");
+
+		br.setTurnCount(0);
 		assertEquals("blue", br.whichTeamWonAssassin());
-		br.setRedTeamTurn(false);
+
 		assertEquals("red", br.whichTeamWonAssassin());
 	}
-	
-	/*
-	 * Checks that gameStart properly runs the methods it uses.
-	 */
-	@Test
-	public void gameStartTest() {
-		Board br = new Board();
-		br.createBoard();
-		br.gameStart("src/GameWords.txt");
-		assertTrue(br.isRedTeamTurn());
-		for(int index = 0; index < 25; index++) {
-			assertNotNull(br.getBoard()[index].getCodename());
-			assertNotNull(br.getBoard()[index].getPerson());
-			assertTrue(br.getBoard()[index].isNotRevealed());
-		}
-	}
-	
+
+	//	/*
+	//	 * Checks that gameStart properly runs the methods it uses.
+	//	 */
+	//	@Test
+	//	public void gameStartTest() {
+	//		Board br = new Board();
+	//		br.createBoard();
+	//		br.gameStart("src/GameWords.txt");
+	//		assertTrue(br.isRedTeamTurn());
+	//		for(int index = 0; index < 25; index++) {
+	//			assertNotNull(br.getBoard()[index].getCodename());
+	//			assertNotNull(br.getBoard()[index].getPerson());
+	//			assertTrue(br.getBoard()[index].isNotRevealed());
+	//		}
+	//	}
+	//	
 	/*
 	 * Checks that createBoard makes a board of 25 locations with the right amount of agents.
 	 */
 	@Test
-	public void createBoardTest() {
+	public void createBoardTestTwoPL() {
 		Board br = new Board();
+		br.setTwoPlayerGame(true);
 		br.createBoard();
 		assertEquals(25, br.getBoard().length);
 		assertEquals(9, br.getRedsLeft());
 		assertEquals(8, br.getBluesLeft());
 	}
-	
+
+	@Test
+	public void createBoardTestTrePL() {
+		Board br = new Board();
+		br.setTwoPlayerGame(false);
+		br.createBoard();
+		assertEquals(25, br.getBoard().length);
+		assertEquals(6, br.getRedsLeft());
+		assertEquals(5, br.getBluesLeft());
+		assertEquals(5, br.getGreensLeft());
+	}
+
 	/*
 	 * Checks that checkGuess returns the proper value in all potential cases. 
 	 */
@@ -184,32 +207,44 @@ public class BoardTest {
 		Board br = new Board();
 		br.createBoard();
 		br.gameStart("src/GameWords.txt");
+		
 		br.getBoard()[3].setCodename("");
 		br.getBoard()[3].setPerson("blue");
-		br.setRedTeamTurn(false);
-		assertTrue(br.checkGuess(""));
+		br.setTurnCount(1);
+		assertTrue("Person = blue, turn = blue",br.checkGuess(""));
+		
 		br.getBoard()[4].setCodename("food");
 		br.getBoard()[4].setPerson("red");
-		br.setRedTeamTurn(false);
-		assertFalse(br.checkGuess("car")); 
+		br.setTurnCount(2);
+		assertFalse("Person = red, turn = green",br.checkGuess("car")); 
+		
 		br.getBoard()[5].setCodename("boolean");
 		br.getBoard()[5].setPerson("red");
-		br.setRedTeamTurn(false);
-		assertFalse(br.checkGuess("boolean"));
+		br.setTurnCount(1);
+		assertFalse("Person = red, turn = blue",br.checkGuess("boolean"));
+		
 		br.getBoard()[3].setCodename("bigMac");
-		br.getBoard()[3].setPerson("blue");
-		br.setRedTeamTurn(true);
-		assertFalse(br.checkGuess("bigMac"));
+		br.getBoard()[3].setPerson("green");
+		br.setTurnCount(0);
+		assertFalse("Person = green, turn = red", br.checkGuess("bigMac"));
+		
+		br.getBoard()[4].setCodename("plane");
+		br.getBoard()[4].setPerson("green");
+		br.setTurnCount(2);
+		assertTrue("Person = green, turn = green",br.checkGuess("bigMac"));
+		
 		br.getBoard()[3].setCodename("");
 		br.getBoard()[3].setPerson("red");
-		br.setRedTeamTurn(true);
-		assertTrue(br.checkGuess("")); 
-		br.getBoard()[3].setPerson("innocent");
-		assertFalse(br.checkGuess(""));
-	    br.getBoard()[2].setPerson("assasin");
-	    assertFalse(br.checkGuess("")); 
+		br.setTurnCount(0);
+		assertTrue("Person = red, turn = red",br.checkGuess("")); 
 		
+		br.getBoard()[3].setPerson("innocent");
+		assertFalse("Person = yellow",br.checkGuess(""));
+		
+		br.getBoard()[2].setPerson("assasin");
+		assertFalse("person = assasin",br.checkGuess("")); 
+
 	}
-	
+
 	// Do not need a test for assignPeople as those methods within it have already been tested elsewhere.
 }
